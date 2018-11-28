1ffae94097fa1f1d09e79fc48d80c0e9731f6314
lchowdhury <chowdhurlobid@gmail.com>
2018-11-28 14:03:42 -0500

    2nd commit

---
+++ Basic project/src/findHouse/TestHouseInfo.java
@@ -0,0 +1,24 @@
+package findHouse;
+
+public class TestHouseInfo {
+
+	public static void main(String[] args) {
+
+		String FirstName = "Tasnia";
+		String LastName = "Nusrat";
+		int HouseNumber = 2074;
+		String HouseName = "Matthews Avenue";
+		String AptNumber = "1st floor";
+		int ZipCode = 10462;
+		String County = "Bronx";
+		String StateName = "New York";
+		String Region = "Bangladesh";
+		Boolean UsCitizen = false;
+		String CellPhoneNumber = "+1929-405-9757";
+
+		System.out.println(FirstName + " " + LastName + " " + HouseName + " " + HouseNumber + " " + AptNumber + " "
+				+ ZipCode + " " + County + " " + StateName + " " + Region + " " + UsCitizen + " " + CellPhoneNumber);
+
+	}
+
+}
